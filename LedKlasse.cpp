a9d62897ab1552ef5e25ebe10a933d37d12e4baa
PClara101 <claripmusik@gmail.com>
2025-03-06 12:26:23 +0100

    Add files via upload

---
+++ LedKlasse.cpp
@@ -0,0 +1,19 @@
+#include "Arduino.h"
+#include "LedKlasse.h"
+LedKlasse::LedKlasse(int pin) {
+  pinMode(pin, OUTPUT);
+  _pin = pin;
+  stateLed = false;
+}
+
+void LedKlasse::blinken(int blinkRate) {
+  digitalWrite(_pin, HIGH);
+  delay(blinkRate);
+  digitalWrite(_pin, LOW);
+  delay(blinkRate);
+}
+
+void LedKlasse::toggle() {
+  stateLed = !stateLed;
+  digitalWrite(_pin, stateLed);
+}
