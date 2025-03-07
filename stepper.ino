b86e9d09486b14e401b321070a16566f730e7367
PClara101 <claripmusik@gmail.com>
2025-03-07 17:51:12 +0100

    Update stepper.ino
    Verwendet jetzt Stepperklasse

--- stepper.ino
+++ stepper.ino
@@ -1,34 +1,30 @@
-int stepCounter;
-int steps = 1000;
+/*
+  STEPPER
+  > Steuererung des Steppermotors 
+  > verwendet stepper Klasse
+    - stepper.cpp und stepper.h einbinden
+*/
+
+
+#include "stepper.h"
+
+#define ENABLE_PIN 4  // wenn HIGH, Motor aus
+#define STEP_PIN 2    //
+#define DIR_PIN 3     //Richtung (high=richtung trichter, low=richtung parkplatz)
+
+int col;
+
+stepper stepper(ENABLE_PIN, STEP_PIN, DIR_PIN);
 
 void setup() {
-  pinMode(4, OUTPUT); // Enable
-  pinMode(2, OUTPUT); // Step
-  pinMode(3, OUTPUT); // Richtung
-
-  digitalWrite(4, LOW);
+  randomSeed(analogRead(2));
+  col = random(1,8);
+  stepper.moveTo(col);
+  delay(2000);
+  stepper.moveTo(0);  // fährt zurück zum Parkplatz
 }
 
 void loop() {
-  digitalWrite(3,HIGH); // im Uhrzeigersinn
+  // put your main code here, to run repeatedly:
 
-  for(stepCounter = 0; stepCounter < steps; stepCounter++) {
-    digitalWrite(2,HIGH);
-    delayMicroseconds(500);
-    digitalWrite(2,LOW);
-    delayMicroseconds(500);
-  }
-  
-  delay(1000);
-
-  digitalWrite(3,LOW); // gegen den Uhrzeigersinn
-
-  for(stepCounter = 0; stepCounter < steps; stepCounter++) {
-    digitalWrite(2,HIGH);
-    delayMicroseconds(500);
-    digitalWrite(2,LOW);
-    delayMicroseconds(500);
-  }
-
-  delay(1000);
 }
