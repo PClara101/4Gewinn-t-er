7eedd637570a9b3616fb80668a936e64d7c6b085
julia-fan-06 <julia4fan@gmail.com>
2025-03-31 20:41:08 +0200

    Update Knoepfe.ino
    spalte 1-7 statt 0-6

--- Knoepfe.ino
+++ Knoepfe.ino
@@ -6,33 +6,33 @@ void setup() {
 
 void loop() {
   int buttonvalue = analogRead(A0);
-// Spalte 0
-  if(buttonvalue <= 155 && buttonvalue > 100) {
-    Serial.println(0);
-  }
 // Spalte 1
-  if(buttonvalue <= 185 && buttonvalue > 160) {
-  Serial.println(1);
+  if(buttonvalue <= 155 && buttonvalue > 100) {
+    Serial.println(1);
   }
 // Spalte 2
-  if(buttonvalue <= 230 && buttonvalue > 190) {
+  if(buttonvalue <= 185 && buttonvalue > 160) {
   Serial.println(2);
   }
 // Spalte 3
-  if(buttonvalue <= 320 && buttonvalue > 240) {
+  if(buttonvalue <= 230 && buttonvalue > 190) {
   Serial.println(3);
   }
 // Spalte 4
-  if(buttonvalue <= 400 && buttonvalue > 340) {
+  if(buttonvalue <= 320 && buttonvalue > 240) {
   Serial.println(4);
   }
 // Spalte 5
-  if(buttonvalue <= 600 && buttonvalue > 490) {
+  if(buttonvalue <= 400 && buttonvalue > 340) {
   Serial.println(5);
   }
 // Spalte 6
-  if(buttonvalue <= 1024 && buttonvalue > 900) {
+  if(buttonvalue <= 600 && buttonvalue > 490) {
   Serial.println(6);
+  }
+// Spalte 7
+  if(buttonvalue <= 1024 && buttonvalue > 900) {
+  Serial.println(7);
   }
   delay(300);
 }
