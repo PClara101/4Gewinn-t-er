b4c52b9be5a917908b38bd399bf4b828b188875d
jckberlin <j.kritz@icloud.com>
2025-03-31 23:09:46 +0200

    Update spiellogik_robotik.ino

--- spiellogik_robotik/spiellogik_robotik.ino
+++ spiellogik_robotik/spiellogik_robotik.ino
@@ -3,15 +3,29 @@
 #define ROWS = 6
 #define COLUMNS = 7
 #define EMPTY = 0
+#define Spieler = 1
+#define SpielLogik = 2
+#define MAX_DEPTH 4
+
 int board[ROWS][COLUMNS];
 
 void setup() {
   Serial.begin(9600);
   Spielfeld();
   MonitorSpielfeld();
+  start();
+
+  for (int rounds = 1, rounds > 20, rounds ++) {
+    knoepfe() {
+    int SpielerZug = col();
+    if (richtigerZug(SpielerZug)) {
+      Zug(SpielerZug, PLAYER);
+      MonitorSpielfeld();
+
+      
 }
 
-void 
+
 
 
 
