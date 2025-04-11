7231adb6c47528af10266641e1e3b3b77f47bf7b
PClara101 <claripmusik@gmail.com>
2025-04-11 13:17:32 +0200

    startEnde.h kommentiert

--- Abgabe Robotik-Labor/spiellogik_robotik/startEnde.h
+++ Abgabe Robotik-Labor/spiellogik_robotik/startEnde.h
@@ -1,3 +1,7 @@
+/*
+  Headerdatei für die Kommuikation am Anfang und am Ende des Spiels
+*/
+
 #ifndef startEnde_h
 #define startEnde_h
 #include "Arduino.h"
@@ -7,6 +11,7 @@ class startEnde{
   public: 
     void start();
     void ende(int gewinnter);
+  // die folgenden Funktionen werden von start() und ende() verwendet:
     void soundStart();
     void losen();
     void accelerando();
